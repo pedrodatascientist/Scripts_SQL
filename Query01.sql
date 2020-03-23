dd494a1f22d08413e32ecec166377ba9823ea5ac
Pedro Henrique <61524050+pedrodatascientist@users.noreply.github.com>
2020-03-23 19:42:17 -0300

    Add files via upload

---
+++ Query01.sql
@@ -0,0 +1,28 @@
+SELECT DimCustomer.FirstName, 
+DimCustomer.LastName, 
+DimCustomer.Gender, 
+DimCustomer.TotalChildren, 
+DimDate.DateKey, 
+DimDate.DayNumberOfMonth, 
+DimDate.EnglishMonthName, 
+DimDate.CalendarYear, 
+DimDate.CalendarSemester, 
+DimDate.CalendarQuarter, 
+DimProduct.Color, 
+DimProductSubcategory.EnglishProductSubcategoryName, 
+DimProductCategory.EnglishProductCategoryName, 
+FactInternetSales.OrderQuantity, 
+FactInternetSales.TotalProductCost, 
+FactInternetSales.SalesAmount, 
+FactInternetSales.ProductStandardCost, 
+FactInternetSales.OrderDate, 
+FactInternetSales.ShipDate, 
+DimProduct.EnglishProductName
+FROM 
+DimDate INNER JOIN
+DimCustomer INNER JOIN
+DimProductSubcategory INNER JOIN
+DimProduct ON DimProductSubcategory.ProductSubcategoryKey = DimProduct.ProductSubcategoryKey INNER JOIN
+DimProductCategory ON DimProductSubcategory.ProductCategoryKey = DimProductCategory.ProductCategoryKey INNER JOIN
+FactInternetSales ON DimProduct.ProductKey = FactInternetSales.ProductKey ON DimCustomer.CustomerKey = FactInternetSales.CustomerKey ON 
+DimDate.DateKey = FactInternetSales.OrderDateKey
